0d592b43fc5c0fdcfded73f04de9399a9beeef9b
Max Gladlov <m.gladkov@avtopass.net>
2022-09-16 14:17:26 +0300

    Initial Commit

---
+++ TDDToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TDDToDoApp
+//
+//  Created by Maxim Gladkov on 16.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TDDToDoAppUITests/TDDToDoAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TDDToDoAppUITestsLaunchTests.swift
+//  TDDToDoAppUITests
+//
+//  Created by Maxim Gladkov on 16.09.2022.
+//
+
+import XCTest
+
+final class TDDToDoAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
